a80275066be7ad66a14bc201ab3fab62c7c3b9b7
Saadnaeem453 <jsaad3663@gmail.com>
2024-08-02 17:47:05 +0500

    create book api

--- server/index.js
+++ server/index.js
@@ -22,7 +22,24 @@ app.get("/books", (req,res)=>{
             return res.json(data)
     })
 })
+app.post("/createBook", (req, res) => {
+    // Why we are not giving the values directly in query , 
+    // beacuse if we will give this ,it will become a query ,
+    //  so a attacker or hacker might be send a malicous query to hack our database , like
+    //  '1' ="1"  this will return true and we will lost our data
+    // so we have used a concept name paramterized queries
 
+    const q = "INSERT INTO books ( `title`, `description`, `cover`) VALUES (?)"
+    const  VALUES = [
+        "A Sharp Mind",
+        "A Book for all peoples which are intersting to grow their personality",
+        "Book"
+    ]
+    db.query(q,[VALUES] ,(err, data)=>{
+        if(err) return res.json(err)
+            return res.json(data)
+    })
+})
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
